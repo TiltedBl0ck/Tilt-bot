8686862fd9a34b5a5787d7237bf66cba51a21a99
TiltedBl0ck <raselcat22@gmail.com>
2025-11-16 09:56:21 +0800

    commit

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/mnt/Storage/Project Z/Project Tilt-bot-main/dev/.venv'
+set -gx VIRTUAL_ENV '/mnt/Storage/Project Z/Project Tilt-bot-main/main/.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
